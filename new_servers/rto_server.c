5aca0b63c4edd09624bce6909ffd084d9a751af5
EC2 Default User <ec2-user@ip-172-31-33-123.us-east-2.compute.internal>
2019-10-16 03:00:17 +0000

    write connected ip to file

--- new_servers/rto_server.c
+++ new_servers/rto_server.c
@@ -147,7 +147,9 @@ int main(int argc, char** argv) {
 		if (inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr, clntName, sizeof(clntName)) != NULL) {
 			printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
 			FILE *fd = fopen("ip.txt","w");
-			fwrite(clntName,sizeof(char),INET_ADDRSTRLEN,fd);
+			//fwrite(clntName,sizeof(char),INET_ADDRSTRLEN,fd);
+			fputs(clntName,fd);
+			fclose(fd);
         }//end if
 		else
 			puts("Unable to get client address");
