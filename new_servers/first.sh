d76fe82e88487e1772ea0b961859abdbde4def12
Josephine <josephinekfchow@gmail.com>
2019-10-19 12:31:23 -0700

    still trying tcpdump

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -6,5 +6,5 @@ rm -f ip.txt
 ping $2 -c 10 > $1_server_ping.txt 
 gcc -pthread rto_server.c -o rto
 nohup ./rto > $1_rto.txt &
-nohup sudo tcpdump host $2 and port 5007 and tcp -w $1_rto_server.pcap &
+nohup sudo tcpdump port 5007 -w $1_rto_server.pcap &
 
