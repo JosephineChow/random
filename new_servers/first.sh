376c2680d1273e579fc2e25e8a07984f2b0eb078
Josephine <josephinekfchow@gmail.com>
2019-10-19 11:39:58 -0700

    debug msg

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -2,6 +2,9 @@
 
 # This script takes 2 arguments, 1) naming convention 2) VPN IP 
 
+echo $1 >> out.txt
+echo $2 >> out.txt
+exit 
 rm -f ip.txt 
 ping $2 -c 100 > $1_server_ping.txt 
 gcc -pthread rto_server.c -o rto >> out.txt 2>>err.txt 
