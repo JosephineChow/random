990028e8fcea47991200c68a88b773881a6e8b8d
Josephine <josephinekfchow@gmail.com>
2019-10-19 12:43:02 -0700

    following instructions..

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -6,5 +6,6 @@ rm -f ip.txt
 ping $2 -c 10 > $1_server_ping.txt 
 gcc -pthread rto_server.c -o rto
 nohup ./rto > $1_rto.txt &
-nohup sudo tcpdump port 5007 -w $1_rto_server.pcap &
+nohup sudo tcpdump port 5007 -w $1_rto_server.pcap >/dev/null 2>&1 &
+sleep 2 
 
