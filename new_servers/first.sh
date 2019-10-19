9b2bf90e83a1131c86fe5086de238a7d8d701499
Josephine <josephinekfchow@gmail.com>
2019-10-19 12:28:02 -0700

    tcpdump issue 2.o

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -2,14 +2,9 @@
 
 # This script takes 2 arguments, 1) naming convention 2) VPN IP 
 
-echo $1 >> out.txt
-echo $2 >> out.txt
-
 rm -f ip.txt 
 ping $2 -c 10 > $1_server_ping.txt 
-gcc -pthread rto_server.c -o rto >> out.txt 2>>err.txt 
-
+gcc -pthread rto_server.c -o rto
 nohup ./rto > $1_rto.txt &
-nohup tcpdump host $2 and port 5007 and tcp -w $1_rto_server.pcap & >> out.txt 2>>err.txt 
-touch post_tcpdump
+nohup sudo tcpdump host $2 and port 5007 and tcp -w $1_rto_server.pcap &
 
--- new_servers/second.sh
+++ new_servers/second.sh
@@ -3,7 +3,6 @@
 # This script takes 2 arguments, 1) naming convention 2) VPN IP 
 
 
-cd random/new_servers
 sudo pkill tcpdump
 nohup python2 server.py &
 sudo nohup tcpdump host $2 and port 5005 and tcp -w $1_server.pcap &
