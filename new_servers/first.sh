bcc7fae760f028effc4f5c66518481ea2ebd67b8
Josephine <josephinekfchow@gmail.com>
2019-10-19 11:57:01 -0700

    making it faster..

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -6,7 +6,7 @@ echo $1 >> out.txt
 echo $2 >> out.txt
 
 rm -f ip.txt 
-ping $2 -c 100 > $1_server_ping.txt 
+ping $2 -c 10 > $1_server_ping.txt 
 gcc -pthread rto_server.c -o rto >> out.txt 2>>err.txt 
 
 nohup ./rto > $1_rto.txt &
