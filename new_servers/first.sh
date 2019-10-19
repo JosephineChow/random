48a4d8d9c1895329548b28cfb848e9c933e89bd2
Josephine <josephinekfchow@gmail.com>
2019-10-19 12:03:45 -0700

    nohup tcpdump?

--- new_servers/first.sh
+++ new_servers/first.sh
@@ -10,4 +10,4 @@ ping $2 -c 10 > $1_server_ping.txt
 gcc -pthread rto_server.c -o rto >> out.txt 2>>err.txt 
 
 nohup ./rto > $1_rto.txt &
-sudo nohup tcpdump host $2 and port 5007 and tcp -w $1_rto_server.pcap &
+nohup tcpdump host $2 and port 5007 and tcp -w $1_rto_server.pcap &
